7e46c7c06a049757b72a4283518df23c65508644
_manecraf <marcioviniciusvoss@gmail.com>
2022-09-19 16:30:47 -0300

    7 aula

--- core/serializers.py
+++ core/serializers.py
@@ -1,9 +1,28 @@
 from rest_framework.serializers import ModelSerializer
 
-from core.models import Categoria
+from core.models import Carro, Categoria, Marca
 
 
 class CategoriaSerializer(ModelSerializer):
     class Meta:
         model = Categoria
         fields = "__all__"
+
+
+class MarcaSerializer(ModelSerializer):
+    class Meta:
+        model = Marca
+        fields = "__all__"
+
+
+class CarroSerializer(ModelSerializer):
+    class Meta:
+        model = Carro
+        fields = "__all__"
+
+
+class CarroDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Carro
+        fields = "__all__"
+        depth = 1
--- core/views.py
+++ core/views.py
@@ -1,9 +1,28 @@
 from rest_framework.viewsets import ModelViewSet
 
-from core.models import Categoria
-from core.serializers import CategoriaSerializer
+from core.models import Carro, Categoria, Marca
+from core.serializers import (
+    CarroSerializer,
+    CarroDetailSerializer,
+    CategoriaSerializer,
+    MarcaSerializer,
+)
 
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
+
+
+class CarroViewSet(ModelViewSet):
+    queryset = Carro.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return CarroDetailSerializer
+        return CarroSerializer
